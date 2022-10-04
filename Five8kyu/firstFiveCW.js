38884aef92aaff1272caff53bf22653400780aac
oscarthecoderr <dev.oscarrios@gmail.com>
2022-10-04 17:17:46 -0400

    more codewar solutions

--- Five8kyu/firstFiveCW.js
+++ Five8kyu/firstFiveCW.js
@@ -38,3 +38,14 @@ function updateLight(current) {
 const quarterOf = (month) => {
   return Math.ceil(month / 3) 
 }
+
+// 6. Even or Odd
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function even_or_odd(number) {
+  if(number % 2 === 0){
+    return 'Even'
+  } else{
+    return 'Odd'
+  }
+}
