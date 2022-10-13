a0f0b9c94fe64537bf2412f681029108126ed5c3
oscarthecoderr <dev.oscarrios@gmail.com>
2022-10-12 21:46:45 -0400

    array plus array solution

--- Five8kyu/firstFiveCW.js
+++ Five8kyu/firstFiveCW.js
@@ -77,3 +77,22 @@ function numberToString(num) {
 function solution(str){
   return str.split('').reverse().join('')
 }
+
+//11. 
+
+
+function removeEveryOther(arr){
+  for (let i = 1; i < arr.length; i++){
+    arr.splice(i,1)     
+  }
+  return arr
+}
+
+
+//12. array plus array
+//I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
+//P.S. Each array includes only integer numbers. Output is a number too.
+
+function arrayPlusArray(arr1, arr2) {
+  return arr1.concat(arr2).reduce((acc, cur) => acc + cur);
+}
