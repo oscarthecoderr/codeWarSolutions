d16c34f8996bce7775960c431a9df29d26e36f78
oscarthecoderr <dev.oscarrios@gmail.com>
2022-10-25 17:43:15 -0400

    String repeat 8kyu

--- Five8kyu/firstFiveCW.js
+++ Five8kyu/firstFiveCW.js
@@ -155,3 +155,10 @@ function positiveSum(arr) {
    //  if its true, add the number to variable answer
 
 // return answer
+
+// 15 . 8kyu
+//Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+function repeatStr (n, s) {
+  
+  return s.repeat(n)
+}
